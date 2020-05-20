03c8a214ac93ebe62f97e7614f01747afeed57d3
Thomas Aldaitz <taldaitz@dawan.fr>
2020-05-20 12:23:55 +0200

    Ajout exo05

---
+++ Exo05/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo05
+{
+    class Customer
+    {
+        public String Nom { get; set; }
+        public String Prenom { get; set; }
+        public String Email { get; set; }
+        public int Age { get; set; }
+
+        public bool IsEmailValid
+        {
+            get
+            {
+                return Email.Contains("@");
+            }
+        }
+    }
+}
