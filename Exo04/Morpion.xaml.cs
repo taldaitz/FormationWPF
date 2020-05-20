e8192f5c7a5307d714ea811d58bb96553e516245
Thomas Aldaitz <taldaitz@dawan.fr>
2020-05-20 08:47:47 +0200

    ajout exo4

---
+++ Exo04/Morpion.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exo04
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int nbCoup = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void JouerCoup(object sender, RoutedEventArgs e)
+        {
+            Button bouton = (Button)sender;
+
+            Grid grid = (Grid)bouton.Parent;
+
+            int index = grid.Children.IndexOf(bouton);
+
+            grid.Children.RemoveAt(index);
+
+            var uriImage = new Uri(nbCoup % 2 == 0 ? @"img/croix.png" : @"img/rond.png", UriKind.Relative);
+
+            Image img = new Image() { Source = new BitmapImage(uriImage) };
+
+            grid.Children.Insert(index, img);
+            Grid.SetRow(img, Grid.GetRow(bouton));
+            Grid.SetColumn(img, Grid.GetColumn(bouton));
+
+            nbCoup ++;
+        }
+    }
+}
---
+++ Revision/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+    class Person
+    {
+        public String Nom { get; set; }
+        public String Prenom { get; set; }
+    }
+}
